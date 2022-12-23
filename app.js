1eaf7bfb1caccdff1a5515b62992a58944dc5a56
fer <fernando452000@gmail.com>
2022-12-23 16:05:19 -0600

    Se agregó un botón nuevo para compartir los mazos, de momento solo funciona en las cartas de hechizo

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ console.log('Hola, todo está funcionando');
 const cards = [];
 
 class Cards {
-    constructor(name, img){
+    constructor(name, img, id){
         this.name = name;
         this.img = img;
+        this.id = id;
     }
 }
 
 // Tropa
-const caballero = new Cards('Caballero','https://cdn.statsroyale.com/images/cards/full/knight.png');
+/* const caballero = new Cards('Caballero','https://cdn.statsroyale.com/images/cards/full/knight.png');
 const arqueras = new Cards('Arqueras','https://cdn.statsroyale.com/images/cards/full/archers.png');
 const duendes = new Cards('Duendes','https://cdn.statsroyale.com/images/cards/full/goblins.png');
 const gigante = new Cards('Gigante','https://cdn.statsroyale.com/images/cards/full/giant.png');
@@ -86,12 +87,11 @@ const dragonesEsqueleto = new Cards('Dragones Esqueleto','https://cdn.statsroyal
 const brujaMadre = new Cards('Bruja Madre','https://cdn.statsroyale.com/images/cards/full/motherwitch.png');
 const espirituElectrico = new Cards('Espíritu Eléctrico','https://cdn.statsroyale.com/images/cards/full/electrospirit.png');
 const giganteElectrico = new Cards('Gigante Eléctrico','https://cdn.statsroyale.com/images/cards/full/electrogiant.png');
-const fenix = new Cards('Fénix','https://cdn.statsroyale.com/images/cards/full/phoenix.png');
-const espirituSanador = new Cards('Espíritu Sanador','https://cdn.statsroyale.com/images/cards/full/healspirit.png');
+const fenix = new Cards('Fénix','https://cdn.statsroyale.com/images/cards/full/phoenix.png'); */
 
 
 //Estructura
-const canon = new Cards('Cañón','https://cdn.statsroyale.com/images/cards/full/chaos_cannon.png');
+/* const canon = new Cards('Cañón','https://cdn.statsroyale.com/images/cards/full/chaos_cannon.png');
 const chozaDeDuendes = new Cards('Choza De Duendes','https://cdn.statsroyale.com/images/cards/full/fire_furnace.png');
 const mortero = new Cards('Mortero','https://cdn.statsroyale.com/images/cards/full/building_mortar.png');
 const torreInfernal = new Cards('Torre Infernal','https://cdn.statsroyale.com/images/cards/full/building_inferno.png');
@@ -103,55 +103,79 @@ const ballesta = new Cards('Ballesta','https://cdn.statsroyale.com/images/cards/
 const lapida = new Cards('Lápida','https://cdn.statsroyale.com/images/cards/full/tombstone.png');
 const horno = new Cards('Horno','https://cdn.statsroyale.com/images/cards/full/firespirit_hut.png');
 const jaulaDelForzudo = new Cards('Jaula Del Forzudo','https://cdn.statsroyale.com/images/cards/full/goblin_cage.png');
-const excavadoraDeDuendes = new Cards('Excavadora De Duendes','https://cdn.statsroyale.com/images/cards/full/goblindrill.png');
+const excavadoraDeDuendes = new Cards('Excavadora De Duendes','https://cdn.statsroyale.com/images/cards/full/goblindrill.png'); */
 
 
 //Hechizo
-const bolaDeFuego = new Cards('Bola De Fuego','https://cdn.statsroyale.com/images/cards/full/fire_fireball.png');
-const flechas = new Cards('Flechas','https://cdn.statsroyale.com/images/cards/full/order_volley.png');
-const furia = new Cards('Furia','https://cdn.statsroyale.com/images/cards/full/rage.png');
-const cohete = new Cards('Cohete','https://cdn.statsroyale.com/images/cards/full/rocket.png');
-const barrilDeDuendes = new Cards('Barril De Duendes','https://cdn.statsroyale.com/images/cards/full/goblin_barrel.png');
-const hielo = new Cards('Hielo','https://cdn.statsroyale.com/images/cards/full/freeze.png');
-const espejo = new Cards('Espejo','https://cdn.statsroyale.com/images/cards/full/mirror.png');
-const rayo = new Cards('Rayo','https://cdn.statsroyale.com/images/cards/full/lightning.png');
-const descarga = new Cards('Descarga','https://cdn.statsroyale.com/images/cards/full/zap.png');
-const veneno = new Cards('Veneno','https://cdn.statsroyale.com/images/cards/full/poison.png');
-const cementerio = new Cards('Cementerio','https://cdn.statsroyale.com/images/cards/full/graveyard.png');
-const elTronco = new Cards('El Tronco','https://cdn.statsroyale.com/images/cards/full/the_log.png');
-const tornado = new Cards('Tornado','https://cdn.statsroyale.com/images/cards/full/tornado.png');
-const clon = new Cards('Clon','https://cdn.statsroyale.com/images/cards/full/copy.png');
-const terremoto = new Cards('Terremoto','https://cdn.statsroyale.com/images/cards/full/earthquake.png');
-const barrilDeBarbaro= new Cards('Barril De Bárbaro','https://cdn.statsroyale.com/images/cards/full/barb_barrel.png');
-const bolaDeNieve = new Cards('Bola De Nieve','https://cdn.statsroyale.com/images/cards/full/snowball.png');
-const paqueteReal = new Cards('Paquete Real','https://cdn.statsroyale.com/images/cards/full/royal_delivery.png');
+const bolaDeFuego = new Cards('Bola De Fuego','https://cdn.statsroyale.com/images/cards/full/fire_fireball.png','28000000');
+const flechas = new Cards('Flechas','https://cdn.statsroyale.com/images/cards/full/order_volley.png','28000001');
+const furia = new Cards('Furia','https://cdn.statsroyale.com/images/cards/full/rage.png','28000002');
+const cohete = new Cards('Cohete','https://cdn.statsroyale.com/images/cards/full/rocket.png','28000003');
+const barrilDeDuendes = new Cards('Barril De Duendes','https://cdn.statsroyale.com/images/cards/full/goblin_barrel.png','28000004');
+const hielo = new Cards('Hielo','https://cdn.statsroyale.com/images/cards/full/freeze.png','28000005');
+const espejo = new Cards('Espejo','https://cdn.statsroyale.com/images/cards/full/mirror.png','28000006');
+const rayo = new Cards('Rayo','https://cdn.statsroyale.com/images/cards/full/lightning.png','28000007');
+const descarga = new Cards('Descarga','https://cdn.statsroyale.com/images/cards/full/zap.png','28000008');
+const veneno = new Cards('Veneno','https://cdn.statsroyale.com/images/cards/full/poison.png','28000009');
+const cementerio = new Cards('Cementerio','https://cdn.statsroyale.com/images/cards/full/graveyard.png','28000010');
+const elTronco = new Cards('El Tronco','https://cdn.statsroyale.com/images/cards/full/the_log.png','28000011');
+const tornado = new Cards('Tornado','https://cdn.statsroyale.com/images/cards/full/tornado.png','28000012');
+const clon = new Cards('Clon','https://cdn.statsroyale.com/images/cards/full/copy.png','28000013');
+const terremoto = new Cards('Terremoto','https://cdn.statsroyale.com/images/cards/full/earthquake.png','28000014');
+const barrilDeBarbaro= new Cards('Barril De Bárbaro','https://cdn.statsroyale.com/images/cards/full/barb_barrel.png','28000015');
+const espirituSanador = new Cards('Espíritu Sanador','https://cdn.statsroyale.com/images/cards/full/healspirit.png','28000016');
+const bolaDeNieve = new Cards('Bola De Nieve','https://cdn.statsroyale.com/images/cards/full/snowball.png','28000017');
+const paqueteReal = new Cards('Paquete Real','https://cdn.statsroyale.com/images/cards/full/royal_delivery.png','28000018');
 
-cards.push(caballero,arqueras,duendes,gigante,pekka,esbirros,globoBombastico,bruja,barbaros,golem,esqueletos,valquiria,ejercitoDeEsqueletos,bombardero,mosquetera,bebeDragon,
+cards.push(/* caballero,arqueras,duendes,gigante,pekka,esbirros,globoBombastico,bruja,barbaros,golem,esqueletos,valquiria,ejercitoDeEsqueletos,bombardero,mosquetera,bebeDragon,
     principe,mago,miniPekka,duendesConLanza,esqueletoGigante,montapuercos,hordaDeEsbirros,magoDeHielo,giganteNoble,guardias,princesa,principeOscuro,trioDeMosqueteras,sabuesoDeLava,
     espirituDeHielo,espirituDeFuego,minero,chispitas,lanzarrocas,lenador,arieteDeBatalla,dragonInfernal,golemDeHielo,megaesbirro,duendeLanzadardos,pandillaDeDuendes,magoElectrico,
     barbarosDeElite,cazador,verdugo,bandida,reclutasReales,brujaNocturna,murcielagos,fantasmaReal,montacarneros,electrocutadores,pillos,canonConRuedas,megacaballero,barrilDeEsqueletos,
     maquinaVoladora,rompemuros,puercosReales,duendeGigante,pescador,arqueroMagico, dragonElectrico,lanzafuegos,granMinero,golemDeElixir,curanderaGuerrera,reyEsqueleto,reinaArquera,
     caballeroDorado,monje,dragonesEsqueleto,brujaMadre,espirituElectrico,giganteElectrico,fenix,espirituSanador,canon,chozaDeDuendes,mortero,torreInfernal,torreBombardera,
-    chozaDeBarbaros,torreTesla,recolectroDeElixir,ballesta,lapida,horno,jaulaDelForzudo,excavadoraDeDuendes,bolaDeFuego,flechas,furia,cohete,barrilDeDuendes,hielo,espejo,
+    chozaDeBarbaros,torreTesla,recolectroDeElixir,ballesta,lapida,horno,jaulaDelForzudo,excavadoraDeDuendes, */bolaDeFuego,flechas,furia,cohete,barrilDeDuendes,hielo,espejo,
     rayo,descarga,veneno,cementerio,elTronco,tornado,clon,terremoto,barrilDeBarbaro,bolaDeNieve,paqueteReal
 );
 
 const randomButton = document.querySelector('.btn-generate-deck');
 const divDeck = document.querySelector('.deck');
+const share = document.querySelector('.share');
 randomButton.addEventListener('click',(startRandom));
 
 function startRandom(){
+    //Eliminando el contenido
     divDeck.innerHTML = '';
+    share.innerHTML = '';
+
+    //Acomoda la lista de manera aleatoria, sin repetirse
     cards.sort(function() {return Math.random() - 0.5});
+
+    //Creando un enlace para compartir el mazo
+    const aShare = document.createElement('a');
+
+    //Creando una imágen 'Compartir'.
+    const imgShare = document.createElement('img');
+
+    //Agregando la ruta para ver la imágen de 'Compartir'
+    imgShare.src = 'https://link.clashroyale.com/deck/assets/build/0.3/images/stamp_deck@2x.png';
+
+    //Iterando solo para obtener los primeras 8 cartas
     for(let i = 0; i<8; i++){
         const img = document.createElement('img');
         img.src = cards[i].img;
         divDeck.appendChild(img);
         console.log(cards[i]);
     }
+    //Agregando la url del link que se va a compartir dependiendo del mazo
+    aShare.href = `https://link.clashroyale.com/deck/en?deck=${cards[0].id};${cards[1].id};${cards[2].id};${cards[3].id};${cards[4].id};${cards[5].id};${cards[6].id};${cards[7].id}`;
+    aShare.target = "_blank";
+
+    //Agregando el enlace al div .share
+    share.appendChild(aShare);
+
+    //Agregando la imágen al enlace
+    aShare.appendChild(imgShare);
 }
 
 console.log(cards.length);
-
-
 /* console.log(cards); */
